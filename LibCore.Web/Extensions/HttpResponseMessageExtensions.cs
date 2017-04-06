2100411c31369b6400f63dd7e926eeeb023f02c3
mizrael <mizrael@yahoo.it>
2017-04-07 00:49:52 +0200

    added extension class

---
+++ LibCore.Web/Extensions/HttpResponseMessageExtensions.cs
@@ -0,0 +1,34 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace LibCore.Web.Extensions
+{
+    public static class HttpResponseMessageExtensions
+    {
+        /// <summary>
+        ///  throws an HttpRequestException if the request is not successful
+        /// </summary>
+        /// <param name="response"></param>
+        /// <returns></returns>
+        public static async Task AssertSuccessfulAsync(this HttpResponseMessage response)
+        {
+            if (response.IsSuccessStatusCode)
+                return;
+
+            var responseContent = await response.Content.ReadAsStringAsync();
+
+            var errorInfo = Newtonsoft.Json.JsonConvert.DeserializeObject<ApiErrorInfoDTO>(responseContent);
+            if (null != errorInfo && !string.IsNullOrWhiteSpace(errorInfo.Message))
+                throw new HttpRequestException(errorInfo.Message);
+            else
+                throw new HttpRequestException("an error has occurred");
+
+        }
+
+        internal class ApiErrorInfoDTO
+        {
+            public string Message;
+            public Models.ApiError[] Details;
+        }
+    }
+}
